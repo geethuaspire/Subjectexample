3b017d74218fb483ce40e86921b56e07aa84c698
geethu_aspire <geethu.gopi@aspiresys.com>
2022-09-29 11:04:58 +0530

    subject implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Subjectexample';
+  subject = new Subject();
+  firstSubscriptionString = '';
+  secondSubscriptionString = '';
+
+  constructor() {
+    this.subject.next('1');
+    this.subject.next('2');
+
+    this.subject.subscribe(value => {
+      console.log(`Subscription 1: ${value}`);
+      this.firstSubscriptionString += `${value}; `;
+    });
+
+    this.subject.next('3');
+    this.subject.next('4');
+
+    this.subject.subscribe(value => {
+      console.log(`Subscription 2: ${value}`);
+      this.secondSubscriptionString += `${value}; `;
+    });
+
+    this.subject.next('5');
+    this.subject.next('6');
+  }
 }
